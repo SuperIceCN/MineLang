9de54e7b1d80b9103119046c494eae1211aef36c
超神的冰凉 <2801621420@qq.com>
2021-10-23 17:38:14 +0800

    实现比较运算节点

---
+++ MineLangCompiler/src/main/java/me/minelang/compiler/lang/nodes/operator/NotEqualOperatorNode.java
@@ -0,0 +1,85 @@
+package me.minelang.compiler.lang.nodes.operator;
+
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import me.minelang.compiler.lang.nodes.MineNode;
+import me.minelang.compiler.lang.types.MineBigDecimal;
+import me.minelang.compiler.lang.types.MineBigInteger;
+import me.minelang.compiler.lang.types.MineNan;
+
+@NodeInfo(language = "MineLang", shortName = "!=", description = "operatorNotEqual")
+@NodeChild(value = "left", type = MineNode.class)
+@NodeChild(value = "right", type = MineNode.class)
+public abstract class NotEqualOperatorNode extends AbstractOperatorNode {
+
+    @Specialization
+    boolean compareBools(boolean a, boolean b) {
+        return a != b;
+    }
+
+    @Specialization
+    boolean compareBytes(byte a, byte b) {
+        return a != b;
+    }
+
+    @Specialization
+    boolean compareShorts(short a, short b) {
+        return a != b;
+    }
+
+    @Specialization
+    boolean compareInts(int a, int b) {
+        return a != b;
+    }
+
+    @Specialization
+    boolean compareLongs(long a, long b) {
+        return a != b;
+    }
+
+    @Specialization
+    boolean compareBigIntegers(MineBigInteger a, MineBigInteger b) {
+        return a.compareTo(b) != 0;
+    }
+
+    @Specialization
+    boolean compareFloats(float a, float b) {
+        return a != b;
+    }
+
+    @Specialization
+    boolean compareDoubles(double a, double b) {
+        return a != b;
+    }
+
+    @Specialization
+    boolean compareBigDecimals(MineBigDecimal a, MineBigDecimal b) {
+        return a.compareTo(b) != 0;
+    }
+
+    @Specialization
+    boolean compareStrings(String a, String b) {
+        return !a.equals(b);
+    }
+
+    @Specialization
+    boolean compareMineNones(MineNode a, MineNode b) {
+        return a != b;
+    }
+
+    /*
+     * Nan永远不相等
+     */
+    @Specialization
+    boolean compareMineNan(MineNan a, MineNan b) {
+        return true;
+    }
+
+    @Fallback
+    @SuppressWarnings("unused")
+    Object compare(Object a, Object b) {
+        return !a.equals(b);
+    }
+}
--- MineLangCompiler/src/test/java/me/minelang/tests/operators/LogicOperatorNodeTest.java
+++ MineLangCompiler/src/test/java/me/minelang/tests/operators/LogicOperatorNodeTest.java
@@ -1,11 +1,10 @@
 package me.minelang.tests.operators;
 
 import com.oracle.truffle.api.Truffle;
+import me.minelang.compiler.lang.nodes.MineNode;
 import me.minelang.compiler.lang.nodes.MineRootNode;
 import me.minelang.compiler.lang.nodes.literial.LiteralNodeFactory;
-import me.minelang.compiler.lang.nodes.operator.LogicalAndOperatorNodeFactory;
-import me.minelang.compiler.lang.nodes.operator.LogicalNotOperatorNodeFactory;
-import me.minelang.compiler.lang.nodes.operator.LogicalOrOperatorNodeFactory;
+import me.minelang.compiler.lang.nodes.operator.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,7 +12,7 @@ public class LogicOperatorNodeTest {
     public boolean logic(boolean isAnd, boolean a, boolean b) {
         var aNode = LiteralNodeFactory.getInstance().createBoolNode(a);
         var bNode = LiteralNodeFactory.getInstance().createBoolNode(b);
-        var addNode = isAnd? LogicalAndOperatorNodeFactory.create(aNode, bNode) : LogicalOrOperatorNodeFactory.create(aNode, bNode);
+        var addNode = isAnd ? LogicalAndOperatorNodeFactory.create(aNode, bNode) : LogicalOrOperatorNodeFactory.create(aNode, bNode);
         var rootNode = new MineRootNode(addNode);
         var callTarget = Truffle.getRuntime().createCallTarget(rootNode);
         return (boolean) callTarget.call();
@@ -22,6 +21,13 @@ public class LogicOperatorNodeTest {
     public boolean logic(boolean a) {
         var aNode = LiteralNodeFactory.getInstance().createBoolNode(a);
         var addNode = LogicalNotOperatorNodeFactory.create(aNode);
+        var rootNode = new MineRootNode(addNode);
+        var callTarget = Truffle.getRuntime().createCallTarget(rootNode);
+        return (boolean) callTarget.call();
+    }
+
+    public boolean equal(boolean reverse, MineNode aNode, MineNode bNode) {
+        var addNode = !reverse ? EqualOperatorNodeFactory.create(aNode, bNode) : NotEqualOperatorNodeFactory.create(aNode, bNode);
         var rootNode = new MineRootNode(addNode);
         var callTarget = Truffle.getRuntime().createCallTarget(rootNode);
         return (boolean) callTarget.call();
@@ -47,5 +53,22 @@ public class LogicOperatorNodeTest {
     public void testNot() {
         Assert.assertTrue(logic(false));
         Assert.assertFalse(logic(true));
+    }
+
+    @Test
+    public void testEqual() {
+        Assert.assertFalse(equal(false, LiteralNodeFactory.getInstance().createBoolNode(true), LiteralNodeFactory.getInstance().createBoolNode(false)));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createBoolNode(true), LiteralNodeFactory.getInstance().createBoolNode(true)));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createBoolNode(false), LiteralNodeFactory.getInstance().createBoolNode(false)));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createStringNode("hello"), LiteralNodeFactory.getInstance().createStringNode("hell!".replace("!", "o"))));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createNumberNode("23"), LiteralNodeFactory.getInstance().createNumberNode("23")));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createNumberNode("2233"), LiteralNodeFactory.getInstance().createNumberNode("2233")));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createNumberNode("5202233"), LiteralNodeFactory.getInstance().createNumberNode("5202233")));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createNumberNode("22.33"), LiteralNodeFactory.getInstance().createNumberNode("22.33")));
+        // 精度不同不应相等
+        Assert.assertFalse(equal(false, LiteralNodeFactory.getInstance().createNumberNode("2.233"), LiteralNodeFactory.getInstance().createNumberNode("2.233000000000000000000000")));
+        Assert.assertFalse(equal(false, LiteralNodeFactory.getInstance().createNumberNode("2.233"), LiteralNodeFactory.getInstance().createNumberNode("2.233000000")));
+        Assert.assertTrue(equal(false, LiteralNodeFactory.getInstance().createNumberNode("2233"), LiteralNodeFactory.getInstance().createNumberNode("2233.00")));
+        Assert.assertFalse(equal(false, LiteralNodeFactory.getInstance().createNumberNode("52052052052052052022332233"), LiteralNodeFactory.getInstance().createNumberNode("5205205202233")));
     }
 }
