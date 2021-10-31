bd0d0d0177d03b47cc5ff0cc6206bf4989c55a67
超神的冰凉 <2801621420@qq.com>
2021-10-31 22:13:38 +0800

    性能测试工具+循环性能测试

---
+++ MineLangCompiler/src/test/java/me/minelang/tests/TestUtil.java
@@ -0,0 +1,59 @@
+package me.minelang.tests;
+
+import org.apache.commons.math3.stat.descriptive.rank.Median;
+import org.apache.commons.math3.stat.descriptive.summary.Sum;
+
+import java.util.Arrays;
+
+public final class TestUtil {
+    public static double warmUpAverage(int executeTime, TestWorker test) {
+        var start = System.currentTimeMillis();
+        for (var i = 0; i < executeTime; i++)
+            test.run();
+        return (System.currentTimeMillis() - start) / ((double) executeTime);
+    }
+
+    public static double warmUpMedian(int executeTime, TestWorker test) {
+        var data = new Median();
+        var times = new double[executeTime];
+        long start;
+        for (var i = 0; i < executeTime; i++) {
+            start = System.currentTimeMillis();
+            test.run();
+            times[i] = System.currentTimeMillis() - start;
+        }
+        data.setData(times);
+        return data.evaluate();
+    }
+
+    public static TestResult warmUpExactly(String name, int executeTime, TestWorker test) {
+        var times = new double[executeTime];
+        long start;
+        for (var i = 0; i < executeTime; i++) {
+            start = System.currentTimeMillis();
+            test.run();
+            times[i] = System.currentTimeMillis() - start;
+        }
+        var sum = new Sum();sum.setData(times);
+        var total = sum.evaluate();
+        var average = total / executeTime;
+        var median = new Median();median.setData(times);
+        return new TestResult(name, times, total, average, median.evaluate());
+    }
+
+    public interface TestWorker {
+        void run();
+    }
+
+    public record TestResult(String name, double[] times,double total, double average, double median){
+        @Override
+        public String toString() {
+            return """
+                    %s 测试结果：
+                    运行次数 %d / 总耗时 %.2fms / 平均耗时 %.2fms / 中位数 %.2fms
+                    详情：
+                    %s
+                    """.formatted(name, times.length, total, average, median, Arrays.toString(times));
+        }
+    }
+}
--- MineLangCompiler/src/test/java/me/minelang/tests/values/EndlessLoopNodeTest.java
+++ MineLangCompiler/src/test/java/me/minelang/tests/values/EndlessLoopNodeTest.java
@@ -1,8 +1,10 @@
 package me.minelang.tests.values;
 
+import me.minelang.tests.TestUtil;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Engine;
 import org.graalvm.polyglot.Value;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class EndlessLoopNodeTest {
@@ -16,25 +18,25 @@ public class EndlessLoopNodeTest {
 
     @Test
     public void testLoop1() {
-        System.out.println(eval("""
+        Assert.assertEquals(5, eval("""
                 a = 0
                 loop {
                     global a
                     if a < 5 a = a + 1 else break a
                 }
-                """));
+                """).asInt());
     }
 
     @Test
     public void testLoop2() {
-        System.out.println(eval("""
+        Assert.assertEquals("none", eval("""
                 loop break
-                """));
+                """).toString());
     }
 
     @Test
     public void testLoop3() {
-        System.out.println(eval("""
+        Assert.assertEquals("280571172992510140037611932413038677189525", eval("""
                 x1 = 1
                 x2 = 1
                 index = 2
@@ -44,8 +46,26 @@ public class EndlessLoopNodeTest {
                     x3 = x1 + x2
                     x1 = x2
                     x2 = x3
-                    if index == 10 break x3
+                    if index == 200 break x3
+                }
+                """).toString());
+    }
+
+    @Test
+    public void testLoop4() {
+        var result = TestUtil.warmUpExactly("O(n)斐波那契数列第30000项", 15, () -> eval("""
+                x1 = 1
+                x2 = 1
+                index = 2
+                loop {
+                    global x1, x2, index
+                    index = index + 1
+                    x3 = x1 + x2
+                    x1 = x2
+                    x2 = x3
+                    if index == 30000 break x3
                 }
                 """));
+        System.out.println(result);
     }
 }
