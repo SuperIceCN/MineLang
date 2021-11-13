f4d6081492cb68144db5d4adaa2fbd1af05aa4b4
超神的冰凉 <2801621420@qq.com>
2021-11-13 14:27:53 +0800

    国际化支持中文

--- MineLangCompiler/src/main/java/me/minelang/compiler/utils/I18NUtil.java
+++ MineLangCompiler/src/main/java/me/minelang/compiler/utils/I18NUtil.java
@@ -1,29 +1,26 @@
 package me.minelang.compiler.utils;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.Properties;
 
 public final class I18NUtil {
     public static Locale locale = Locale.getDefault();
-    public static Properties languageRes = new Properties();
+    public static JsonObject languageRes;
 
     static {
-        var stream = I18NUtil.class.getResourceAsStream("languages/" + locale.toLanguageTag() + ".properties");
+        var stream = I18NUtil.class.getResourceAsStream("/languages/" + locale.toLanguageTag() + ".json");
         if (stream == null) {
-            stream = I18NUtil.class.getResourceAsStream("languages/en-US.properties");
+            stream = I18NUtil.class.getResourceAsStream("/languages/en-US.json");
         }
         var reader = new BufferedReader(
                 new InputStreamReader(Objects.requireNonNull(stream), StandardCharsets.UTF_8));
-        try {
-            languageRes.load(reader);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        languageRes = JsonParser.parseReader(reader).getAsJsonObject();
     }
 
     /**
@@ -33,7 +30,7 @@ public final class I18NUtil {
      * @return 国际化后内容
      */
     public static String get(String id, Object... args) {
-        var str = languageRes.getProperty(id);
+        var str = languageRes.get(id).getAsString();
         for (var i = 0; i < args.length; i++) {
             str = str.replaceAll("%" + i, args[i].toString());
         }
