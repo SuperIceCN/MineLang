b69f8f2e8a09421b37a9e9e79bcf8ded7bb16236
超神的冰凉 <2801621420@qq.com>
2021-11-21 22:15:16 +0800

    修复压缩脚本，发布新版本

--- .github/scripts/zip.sh
+++ .github/scripts/zip.sh
@@ -1,8 +1,9 @@
 cd "${1}" || exit
+ls
 mkdir "./zipped"
 for file in ./*;
 do
   if [ "$file" != "zipped" ]; then
-      zip -r "./zipped/$file.zip" "$file/*"
+      zip -r "./zipped/$file.zip" "$file"
   fi
 done
